40777df1eabe1bf710bc338f5f404cc606d2551d
Konashuk <konan4ukv@gmail.com>
2023-09-11 20:46:50 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', dataInput);
+formEl.addEventListener('submit', resetForm);
+
+const L_KeyEm = 'feedback-form-email';
+const L_KeyMes = 'feedback-form-message';
+
+const savedEmail = localStorage.getItem(L_KeyEm);
+const savedMessage = localStorage.getItem(L_KeyMes);
+
+function dataInput(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  localStorage.setItem(L_KeyMes, message);
+  localStorage.setItem(L_KeyEm, email);
+}
+
+formEl.elements.email.value = savedEmail || '';
+formEl.elements.message.value = savedMessage || '';
+
+function resetForm(event) {
+  event.preventDefault();
+  localStorage.removeItem(L_KeyEm, L_KeyMes);
+  formEl.reset();
+}
