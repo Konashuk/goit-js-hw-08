798736515a89a046d4f46c06f766074773935514
Konashuk <konan4ukv@gmail.com>
2023-09-11 23:43:04 +0300

    task3 0.2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,26 +4,24 @@ const formEl = document.querySelector('.feedback-form');
 formEl.addEventListener('input', dataInput);
 formEl.addEventListener('submit', resetForm);
 
-const L_KeyEm = 'feedback-form-email';
-const L_KeyMes = 'feedback-form-message';
+const L_Key = 'feedback-form-state';
 
-const savedEmail = localStorage.getItem(L_KeyEm);
-const savedMessage = localStorage.getItem(L_KeyMes);
+const savedData = localStorage.getItem(L_Key);
+const { email, message } = JSON.parse(savedData || '{}');
 
+formEl.elements.email.value = email || '';
+formEl.elements.message.value = message || '';
+console.log(JSON.parse(savedData));
 function dataInput(event) {
   event.preventDefault();
   const form = event.currentTarget;
   const email = form.elements.email.value;
   const message = form.elements.message.value;
-  localStorage.setItem(L_KeyMes, message);
-  localStorage.setItem(L_KeyEm, email);
+  localStorage.setItem(L_Key, JSON.stringify({ email, message }));
 }
-
-formEl.elements.email.value = savedEmail || '';
-formEl.elements.message.value = savedMessage || '';
 
 function resetForm(event) {
   event.preventDefault();
-  localStorage.removeItem(L_KeyEm, L_KeyMes);
+  localStorage.removeItem(L_Key);
   formEl.reset();
 }
